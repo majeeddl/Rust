f8eeed8321ffe9706e3fa95fb0d86344450f3237
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2024-06-14 14:33:48 +0300

    feat: initialize rust file

---
+++ tutorials/primitives/primitives.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let logical: bool = true;
+
+    println!("{}", logical);
+
+    let a_float: f64 = 1.0; // Regular annotation
+
+    let an_integer = 5i32; // Suffix annotation
+
+    let default_float = 3.0; // Inferred type
+
+    let default_integer = 7; // Inferred type
+}
